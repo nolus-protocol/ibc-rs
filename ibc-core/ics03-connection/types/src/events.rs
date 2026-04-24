b10669c197b4d8ce234644f549ff49eb2c799590
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2026-04-24 13:28:53 +0300

    refactor: Use macros to define ICS3 attributes and events.

--- ibc-core/ics03-connection/types/src/events.rs
+++ ibc-core/ics03-connection/types/src/events.rs
@@ -1,306 +1,79 @@
 //! Types for the IBC events emitted from Tendermint Websocket by the connection module.
 
-use ibc_core_host_types::identifiers::{ClientId, ConnectionId};
+use data_types_macros::{define_attribute, define_event};
+use ibc_core_host_types::{
+    error::DecodingError,
+    identifiers::{ClientId, ConnectionId},
+};
 use ibc_primitives::prelude::*;
 use tendermint::abci;
 
-/// Connection event types
-pub const CONNECTION_OPEN_INIT_EVENT: &str = "connection_open_init";
-pub const CONNECTION_OPEN_TRY_EVENT: &str = "connection_open_try";
-pub const CONNECTION_OPEN_ACK_EVENT: &str = "connection_open_ack";
-pub const CONNECTION_OPEN_CONFIRM_EVENT: &str = "connection_open_confirm";
+define_attribute!(
+    "connection_id" => ConnectionIdAttribute(ConnectionId) {
+        friendly_name = "connection ID",
+        into = String::from,
+        try_from = |connection_id: &str| connection_id.parse().map_err(Into::into),
+    }
+);
 
-/// The content of the `key` field for the attribute containing the connection identifier.
-pub const CONN_ID_ATTRIBUTE_KEY: &str = "connection_id";
-pub const CLIENT_ID_ATTRIBUTE_KEY: &str = "client_id";
-pub const COUNTERPARTY_CONN_ID_ATTRIBUTE_KEY: &str = "counterparty_connection_id";
-pub const COUNTERPARTY_CLIENT_ID_ATTRIBUTE_KEY: &str = "counterparty_client_id";
+define_attribute!(
+    "client_id" => ClientIdAttribute(ClientId) {
+        friendly_name = "connection ID",
+        into = String::from,
+        try_from = |client_id: &str| client_id.parse().map_err(Into::into),
+    }
+);
 
-#[cfg_attr(
-    feature = "parity-scale-codec",
-    derive(
-        parity_scale_codec::Encode,
-        parity_scale_codec::Decode,
-        scale_info::TypeInfo
-    )
-)]
-#[cfg_attr(
-    feature = "borsh",
-    derive(borsh::BorshSerialize, borsh::BorshDeserialize)
-)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct Attributes {
-    pub connection_id: ConnectionId,
-    pub client_id: ClientId,
-    pub counterparty_connection_id: Option<ConnectionId>,
-    pub counterparty_client_id: ClientId,
-}
+define_attribute!(
+    "counterparty_connection_id" => CounterpartyConnectionIdAttribute(ConnectionId) {
+        friendly_name = "connection ID",
+        into = String::from,
+        try_from = |connection_id: &str| connection_id.parse().map_err(Into::into),
+    }
+);
 
-/// Convert attributes to Tendermint ABCI tags
-impl From<Attributes> for Vec<abci::EventAttribute> {
-    fn from(a: Attributes) -> Self {
-        let conn_id = (CONN_ID_ATTRIBUTE_KEY, a.connection_id.as_str()).into();
-        let client_id = (CLIENT_ID_ATTRIBUTE_KEY, a.client_id.as_str()).into();
+define_attribute!(
+    "counterparty_client_id" => CounterpartyClientIdAttribute(ClientId) {
+        friendly_name = "connection ID",
+        into = String::from,
+        try_from = |client_id: &str| client_id.parse().map_err(Into::into),
+    }
+);
 
-        let counterparty_conn_id = (
-            COUNTERPARTY_CONN_ID_ATTRIBUTE_KEY,
-            a.counterparty_connection_id
-                .as_ref()
-                .map(|id| id.as_str())
-                .unwrap_or(""),
-        )
-            .into();
+define_event!(
+    "connection_open_init" => OpenInit {
+        conn_id_on_a: ConnectionIdAttribute,
+        client_id_on_a: ClientIdAttribute,
+        client_id_on_b: CounterpartyClientIdAttribute,
+    }
+);
 
-        let counterparty_client_id = (
-            COUNTERPARTY_CLIENT_ID_ATTRIBUTE_KEY,
-            a.counterparty_client_id.as_str(),
-        )
-            .into();
+define_event!(
+    "connection_open_try" => OpenTry {
+        conn_id_on_a: ConnectionIdAttribute,
+        client_id_on_a: ClientIdAttribute,
+        conn_id_on_b: CounterpartyConnectionIdAttribute,
+        client_id_on_b: CounterpartyClientIdAttribute,
+    }
+);
 
-        vec![
-            conn_id,
-            client_id,
-            counterparty_client_id,
-            counterparty_conn_id,
-        ]
+define_event!(
+    "connection_open_ack" => OpenAck {
+        conn_id_on_a: ConnectionIdAttribute,
+        client_id_on_a: ClientIdAttribute,
+        conn_id_on_b: CounterpartyConnectionIdAttribute,
+        client_id_on_b: CounterpartyClientIdAttribute,
     }
-}
+);
 
-#[cfg_attr(
-    feature = "parity-scale-codec",
-    derive(
-        parity_scale_codec::Encode,
-        parity_scale_codec::Decode,
-        scale_info::TypeInfo
-    )
-)]
-#[cfg_attr(
-    feature = "borsh",
-    derive(borsh::BorshSerialize, borsh::BorshDeserialize)
-)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct OpenInit(Attributes);
-
-impl OpenInit {
-    /// Per our convention, this event is generated on chain A.
-    pub fn new(
-        conn_id_on_a: ConnectionId,
-        client_id_on_a: ClientId,
-        client_id_on_b: ClientId,
-    ) -> Self {
-        Self(Attributes {
-            connection_id: conn_id_on_a,
-            client_id: client_id_on_a,
-            counterparty_connection_id: None,
-            counterparty_client_id: client_id_on_b,
-        })
+define_event!(
+    "connection_open_confirm" => OpenConfirm {
+        conn_id_on_a: ConnectionIdAttribute,
+        client_id_on_a: ClientIdAttribute,
+        conn_id_on_b: CounterpartyConnectionIdAttribute,
+        client_id_on_b: CounterpartyClientIdAttribute,
     }
-
-    pub fn conn_id_on_a(&self) -> &ConnectionId {
-        &self.0.connection_id
-    }
-    pub fn client_id_on_a(&self) -> &ClientId {
-        &self.0.client_id
-    }
-    pub fn conn_id_on_b(&self) -> Option<&ConnectionId> {
-        self.0.counterparty_connection_id.as_ref()
-    }
-    pub fn client_id_on_b(&self) -> &ClientId {
-        &self.0.counterparty_client_id
-    }
-
-    pub fn event_type(&self) -> &str {
-        CONNECTION_OPEN_INIT_EVENT
-    }
-}
-
-impl From<OpenInit> for abci::Event {
-    fn from(v: OpenInit) -> Self {
-        abci::Event {
-            kind: CONNECTION_OPEN_INIT_EVENT.to_string(),
-            attributes: v.0.into(),
-        }
-    }
-}
-
-#[cfg_attr(
-    feature = "parity-scale-codec",
-    derive(
-        parity_scale_codec::Encode,
-        parity_scale_codec::Decode,
-        scale_info::TypeInfo
-    )
-)]
-#[cfg_attr(
-    feature = "borsh",
-    derive(borsh::BorshSerialize, borsh::BorshDeserialize)
-)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct OpenTry(Attributes);
-
-impl OpenTry {
-    /// Per our convention, this event is generated on chain B.
-    pub fn new(
-        conn_id_on_b: ConnectionId,
-        client_id_on_b: ClientId,
-        conn_id_on_a: ConnectionId,
-        client_id_on_a: ClientId,
-    ) -> Self {
-        Self(Attributes {
-            connection_id: conn_id_on_b,
-            client_id: client_id_on_b,
-            counterparty_connection_id: Some(conn_id_on_a),
-            counterparty_client_id: client_id_on_a,
-        })
-    }
-
-    pub fn conn_id_on_b(&self) -> &ConnectionId {
-        &self.0.connection_id
-    }
-    pub fn client_id_on_b(&self) -> &ClientId {
-        &self.0.client_id
-    }
-    pub fn conn_id_on_a(&self) -> Option<&ConnectionId> {
-        self.0.counterparty_connection_id.as_ref()
-    }
-    pub fn client_id_on_a(&self) -> &ClientId {
-        &self.0.counterparty_client_id
-    }
-
-    pub fn event_type(&self) -> &str {
-        CONNECTION_OPEN_TRY_EVENT
-    }
-}
-
-impl From<OpenTry> for abci::Event {
-    fn from(v: OpenTry) -> Self {
-        abci::Event {
-            kind: CONNECTION_OPEN_TRY_EVENT.to_string(),
-            attributes: v.0.into(),
-        }
-    }
-}
-
-#[cfg_attr(
-    feature = "parity-scale-codec",
-    derive(
-        parity_scale_codec::Encode,
-        parity_scale_codec::Decode,
-        scale_info::TypeInfo
-    )
-)]
-#[cfg_attr(
-    feature = "borsh",
-    derive(borsh::BorshSerialize, borsh::BorshDeserialize)
-)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct OpenAck(Attributes);
-
-impl OpenAck {
-    /// Per our convention, this event is generated on chain A.
-    pub fn new(
-        conn_id_on_a: ConnectionId,
-        client_id_on_a: ClientId,
-        conn_id_on_b: ConnectionId,
-        client_id_on_b: ClientId,
-    ) -> Self {
-        Self(Attributes {
-            connection_id: conn_id_on_a,
-            client_id: client_id_on_a,
-            counterparty_connection_id: Some(conn_id_on_b),
-            counterparty_client_id: client_id_on_b,
-        })
-    }
-
-    pub fn conn_id_on_a(&self) -> &ConnectionId {
-        &self.0.connection_id
-    }
-    pub fn client_id_on_a(&self) -> &ClientId {
-        &self.0.client_id
-    }
-    pub fn conn_id_on_b(&self) -> Option<&ConnectionId> {
-        self.0.counterparty_connection_id.as_ref()
-    }
-    pub fn client_id_on_b(&self) -> &ClientId {
-        &self.0.counterparty_client_id
-    }
-
-    pub fn event_type(&self) -> &str {
-        CONNECTION_OPEN_ACK_EVENT
-    }
-}
-
-impl From<OpenAck> for abci::Event {
-    fn from(v: OpenAck) -> Self {
-        abci::Event {
-            kind: CONNECTION_OPEN_ACK_EVENT.to_string(),
-            attributes: v.0.into(),
-        }
-    }
-}
-
-#[cfg_attr(
-    feature = "parity-scale-codec",
-    derive(
-        parity_scale_codec::Encode,
-        parity_scale_codec::Decode,
-        scale_info::TypeInfo
-    )
-)]
-#[cfg_attr(
-    feature = "borsh",
-    derive(borsh::BorshSerialize, borsh::BorshDeserialize)
-)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct OpenConfirm(Attributes);
-
-impl OpenConfirm {
-    /// Per our convention, this event is generated on chain B.
-    pub fn new(
-        conn_id_on_b: ConnectionId,
-        client_id_on_b: ClientId,
-        conn_id_on_a: ConnectionId,
-        client_id_on_a: ClientId,
-    ) -> Self {
-        Self(Attributes {
-            connection_id: conn_id_on_b,
-            client_id: client_id_on_b,
-            counterparty_connection_id: Some(conn_id_on_a),
-            counterparty_client_id: client_id_on_a,
-        })
-    }
-
-    pub fn conn_id_on_b(&self) -> &ConnectionId {
-        &self.0.connection_id
-    }
-    pub fn client_id_on_b(&self) -> &ClientId {
-        &self.0.client_id
-    }
-    pub fn conn_id_on_a(&self) -> Option<&ConnectionId> {
-        self.0.counterparty_connection_id.as_ref()
-    }
-    pub fn client_id_on_a(&self) -> &ClientId {
-        &self.0.counterparty_client_id
-    }
-
-    pub fn event_type(&self) -> &str {
-        CONNECTION_OPEN_CONFIRM_EVENT
-    }
-}
-
-impl From<OpenConfirm> for abci::Event {
-    fn from(v: OpenConfirm) -> Self {
-        abci::Event {
-            kind: CONNECTION_OPEN_CONFIRM_EVENT.to_string(),
-            attributes: v.0.into(),
-        }
-    }
-}
+);
 
 #[cfg(test)]
 mod tests {
@@ -330,34 +103,24 @@ mod tests {
         let expected_keys = vec![
             "connection_id",
             "client_id",
-            "counterparty_client_id",
             "counterparty_connection_id",
-        ];
-        let expected_values = vec![
-            "connection-0",
-            "07-tendermint-0",
-            "07-tendermint-1",
-            "connection-1",
+            "counterparty_client_id",
         ];
 
         let tests: Vec<Test> = vec![
             Test {
-                kind: CONNECTION_OPEN_INIT_EVENT,
+                kind: OpenInit::EVENT_KIND,
                 event: OpenInit::new(
                     conn_id_on_a.clone(),
                     client_id_on_a.clone(),
                     client_id_on_b.clone(),
                 )
                 .into(),
-                expected_keys: expected_keys.clone(),
-                expected_values: expected_values
-                    .iter()
-                    .enumerate()
-                    .map(|(i, v)| if i == 3 { "" } else { v })
-                    .collect(),
+                expected_keys: vec!["connection_id", "client_id", "counterparty_client_id"],
+                expected_values: vec!["connection-0", "07-tendermint-0", "07-tendermint-1"],
             },
             Test {
-                kind: CONNECTION_OPEN_TRY_EVENT,
+                kind: OpenTry::EVENT_KIND,
                 event: OpenTry::new(
                     conn_id_on_b.clone(),
                     client_id_on_b.clone(),
@@ -366,10 +129,15 @@ mod tests {
                 )
                 .into(),
                 expected_keys: expected_keys.clone(),
-                expected_values: expected_values.iter().rev().copied().collect(),
+                expected_values: vec![
+                    "connection-1",
+                    "07-tendermint-1",
+                    "connection-0",
+                    "07-tendermint-0",
+                ],
             },
             Test {
-                kind: CONNECTION_OPEN_ACK_EVENT,
+                kind: OpenAck::EVENT_KIND,
                 event: OpenAck::new(
                     conn_id_on_a.clone(),
                     client_id_on_a.clone(),
@@ -378,14 +146,24 @@ mod tests {
                 )
                 .into(),
                 expected_keys: expected_keys.clone(),
-                expected_values: expected_values.clone(),
+                expected_values: vec![
+                    "connection-0",
+                    "07-tendermint-0",
+                    "connection-1",
+                    "07-tendermint-1",
+                ],
             },
             Test {
-                kind: CONNECTION_OPEN_CONFIRM_EVENT,
+                kind: OpenConfirm::EVENT_KIND,
                 event: OpenConfirm::new(conn_id_on_b, client_id_on_b, conn_id_on_a, client_id_on_a)
                     .into(),
                 expected_keys: expected_keys.clone(),
-                expected_values: expected_values.iter().rev().copied().collect(),
+                expected_values: vec![
+                    "connection-1",
+                    "07-tendermint-1",
+                    "connection-0",
+                    "07-tendermint-0",
+                ],
             },
         ];
 
